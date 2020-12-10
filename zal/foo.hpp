8dbb19757dc02e71e4281d36c4b2f1c32cfaa698
zakfilip <70323056+zakfilip@users.noreply.github.com>
2020-12-10 13:24:32 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,7 +10,7 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
     
-    std::for_each(people.cbegin(), people.cend(), [&](Human& h) { h.birthday(); });
+    std::for_each(people.cbegin(), people.cend(), [](Human& h) { h.birthday(); });
     std::transform(people.cbegin(), people.cend(), ret_v.rbegin(), [](const Human& h) {return h.isMonster() ? 'n' : 'y';});
 
     return ret_v;
